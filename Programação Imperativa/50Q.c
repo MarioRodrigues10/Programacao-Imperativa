8fdeed09370b9cc3319ed8aa3ad492cb9cca0d78
Mário Rodrigues <93675410+MarioRodrigues10@users.noreply.github.com>
2022-04-06 21:55:02 +0100

    commit 'small correction on ex45'

--- Programação Imperativa/50Q.c
+++ Programação Imperativa/50Q.c
@@ -601,15 +601,20 @@ int intersectMSet (int N, int v1[N], int v2[N],int r[N]) {
 }
 
 // 45
-int unionMSet (int N, int v1[N], int v2[N], int r[N]) {
-    int count = 0;
-    for(int i = 0; i < N; i++) {
-        r[i] = v1[i] + v2[i];
-        count += r[i]; 
-    }
-    return count;
+int unionMSet (int N, int v1[N], int v2[N], int r[N]){
+	int i;
+	int res = 0;
+	for (i = 0; i < N; i++){
+		if (v1[i] > v2[i]){
+			r[i] = v1[i];
+			res += v1[i];
+		} else {
+			r[i] = v2[i];
+			res += v2[i];
+		}
+	}
+	return res;
 }
-
 // 46
 int cardinalMSet (int N, int v[N]) {
     int count = 0;
